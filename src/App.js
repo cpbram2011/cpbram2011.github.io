9166ee7da2dfb097f93dd358c832897408cf6a9d
cpbram2011 <cpbram2011@gmail.com>
2024-03-20 14:50:21 -0500

    tweak background and fadeIn delay

--- src/App.js
+++ src/App.js
@@ -3,48 +3,34 @@ import logo from './logo.svg';
 import './App.css';
 import NavBar from './components/Navbar';
 import Body from './components/Body'
+import Background from './components/Background';
 
 function App() {
-  const [page, setPage] = useState("")
   const homeRef = useRef(null)
   const aboutRef = useRef(null)
   const skillsRef = useRef(null)
   const projectsRef = useRef(null)
   const contactRef = useRef(null)
 
-  const [x, setX] = useState('50%');
-  const [y, setY] = useState('50%');
 
-  const handleMouseMove = (e) => {
-    const rect = e.target.getBoundingClientRect();
-    const clientX = e.clientX - rect.left;
-    const clientY = e.clientY - rect.top;
-
-    setX(`${clientX}px`);
-    setY(`${clientY}px`);
-  };
 
   useEffect(() => {
-    if (page === "Home") homeRef.current.scrollIntoView({ behavior: 'smooth', block: 'start' });
-    if (page === "About") aboutRef.current.scrollIntoView({ behavior: 'smooth', block: 'start' });
-    if (page === 'Skills') skillsRef.current.scrollIntoView({ behavior: 'smooth', block: 'start' });
-    if (page === 'Projects') projectsRef.current.scrollIntoView({ behavior: 'smooth', block: 'start' });
-    if (page === "Contact") contactRef.current.scrollIntoView({ behavior: 'smooth', block: 'start' });
     document.addEventListener('mousemove', (e) => {
       const overlay = document.querySelector('.overlay');
       overlay.style.setProperty('--x', `${e.clientX}px`);
       overlay.style.setProperty('--y', `${e.clientY}px`);
     });
-    setPage("")
-  }, [page])
+  }, [])
 
   return (
+    <>
+    <Background />
     <div className="App">
       <div className="overlay" />
-        
-        <NavBar page={page} setPage={setPage} />
+        <NavBar refs={{ homeRef, aboutRef, skillsRef, projectsRef, contactRef }}/>
         <Body refs={{homeRef, aboutRef, skillsRef, projectsRef, contactRef}} />
     </div>
+    </>
   );
 }
 
